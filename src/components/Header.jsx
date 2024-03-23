3a06f31730918b96f53a8e2049c7cc0d57ded639
semisweetdemon <semisweetdemon@gmail.com>
2024-03-23 15:15:22 +0600

    header and icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import { logo } from './icons';
+import { Link } from 'react-router-dom';
+import { logo, wishlist, bag, profile, search, call } from './icons';
 
 export const Header = () => {
 	return (
 		<header id="header">
 			<div className="container">
 				<div className="header">
-					<div className="header__top">{logo}</div>
+					<div className="header__top">
+						<div className="logo">
+							{logo}
+							<nav>
+								<Link>Главная</Link>
+								<Link>О нас</Link>
+								<Link>Контакты</Link>
+							</nav>
+						</div>
+						<div className="search">
+							{search}
+							<input type="text" placeholder="Поиск" />
+						</div>
+						<div className="delivery">
+							{call}
+							<Link>8 (964) 89 99 119</Link>
+						</div>
+						<div className="profile">
+							{wishlist}
+							{bag}
+							{profile}
+						</div>
+					</div>
 					<div className="header__navigation"></div>
 				</div>
 			</div>
--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Home from '../pages/Home';
 import { Route, Routes } from 'react-router-dom';
+import CategoryPage from '../pages/CategoryPage';
 
 const MainRouter = () => {
-	const PAGES = [{ path: '/', element: <Home />, key: 1 }];
+	const PAGES = [
+		{ path: '/', element: <Home />, key: 1 },
+		{ path: '/category', element: <CategoryPage />, key: 2 },
+	];
 	return (
 		<Routes>
 			{PAGES.map((el) => (
