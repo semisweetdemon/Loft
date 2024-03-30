45c61ed483eb11530d4fd3fb176963af492a76f2
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-03-30 15:51:51 +0600

    chat

--- src/components/Massage.jsx
+++ src/components/Massage.jsx
@@ -1,17 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const Massage = (el) => {
-
+const Massage = () => {
+  const [massage, setMassage] = useState([])
+  async function readMassage() {
+    let {data} = await axios('http://localhost:3000/massage')
+    setMassage(data)
+  }
+  useEffect(() => {
+    readMassage()
+  }, [])
+  function randomColor() {
+    let color = '#'
+    for(let i = 0; i < 6; i++) {
+      color += Math.floor(Math.random() * 10)
+    }
+    return color 
+  }
   return (
     <div className="massage">
-      <div className="person">
-        <h2>{el.PersonName}</h2>
-        <h3>{el.PersonMail}</h3>
-      </div>
-      <div className="chat">
-        <p>
-          {el.PersMassage}
-        </p>
-      </div>
+      {
+        massage.map((el, idx) => (
+          <div className="user" key={idx}>
+            <div className="avatar" style={{
+              background: randomColor()
+            }}>
+              <h1>{el.PersonName.slice(0, 1)}</h1>
+            </div>
+            <div className="person">
+              <h2>Имя: {el.PersonName}</h2>
+              <h3>E-maill: {el.PersonMail}</h3>
+            </div>
+            <div className="line"></div>
+            <div className="chat">
+              <h5>Cooбщение:</h5>
+              <p>
+                {el.PersMassage}
+              </p>
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 };
